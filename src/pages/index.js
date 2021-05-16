fd17cb8c90d3112bc55d48b7a56136d0226d69e4
Rubén <rupacrod@gmail.com>
2021-05-16 17:38:05 +0200

    Part three

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
+    <Layout>
       <h1>Hello gatsby!</h1>
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
+    </Layout>
   )
 }
